0b452712e64c6e948bc1858724da9ebd9dff5060
marcvegas <marcvegasprv@protonmail.com>
2022-07-24 15:36:57 +0800

    feat: Added active states for sidebar buttons

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,37 +1,28 @@
 <script setup>
+import SidebarButton from './SidebarButton.vue';
 
 
 </script>
 
 <template>
-<nav class="fixed border-gray-500 border-r-2 bg-inherit sm:bottom-0 sm:w-full sm:h-20 
-    lg:h-full top-0 lg:w-20 overflow-auto">
-    <ul class="flex flex-col list-none p-0 m-0 items-center h-full">
-        <li class="w-full">
-            <a href="" class="flex items-center h-20 no-underline">
-                <ViewDashboard/>
-            </a>
-        </li>
-        <li class="w-full">
-            <a href="" class="flex items-center h-20 no-underline">
-                <Feather/>
-            </a>
-        </li>
-        <li class="w-full">
-            <a href="" class="flex items-center h-20 no-underline">
-                <Poll/>
-            </a>
-        </li>
-        <li class="w-full">
-            <a href="" class="flex items-center h-20 no-underline">
-                <AccountCog/>
-            </a>
-        </li>
-        <li class="w-full">
-            <a href="" class="flex items-center h-20 no-underline">
-                <TuneVariant/>
-            </a>
-        </li>
+<nav class="fixed border-gray-500 border-t-[1px] lg:border-r-[1px] bottom-0 w-full h-20 
+    lg:h-full lg:top-0 lg:w-20 overflow-auto">
+    <ul class="flex lg:flex-col flex-row gap-[1em] list-none p-2 m-0 items-center h-full">
+        <SidebarButton>
+            <ViewDashboard :class="['mx-[1.2rem]']" fillColor="#fff"/>
+        </SidebarButton>
+        <SidebarButton>
+            <Feather :class="['mx-[1.2rem]']" fillColor="#fff"/>
+        </SidebarButton>
+        <SidebarButton>
+            <Poll :class="['mx-[1.2rem]']" fillColor="#fff"/>
+        </SidebarButton>
+        <SidebarButton>
+            <AccountCog :class="['mx-[1.2rem]']" fillColor="#fff"/>
+        </SidebarButton>
+        <SidebarButton>
+            <TuneVariant :class="['mx-[1.2rem]']" fillColor="#fff"/>
+        </SidebarButton>
     </ul>
 </nav>
 </template>
--- src/components/SidebarButton.vue
+++ src/components/SidebarButton.vue
@@ -3,5 +3,11 @@
 </script>
 
 <template>
-
+    <li class="w-full">
+        <button
+            class="flex items-center sm:justify-center h-16 no-underline transition-colors ease-in
+            focus:border-2 rounded-lg focus:bg-[#40B67E] focus:border-white">
+            <slot></slot>
+        </button>
+    </li>
 </template>
