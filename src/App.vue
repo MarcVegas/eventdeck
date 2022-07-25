302ecedbc650864a064dba285c91a31971250c9d
marcvegas <marcvegasprv@protonmail.com>
2022-07-25 21:16:14 +0800

    Added Dashboard component to slot

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,13 @@
 import SideBar from './components/SideBar.vue';
 import MainBody from './components/MainBody.vue';
 import Navbar from './components/Navbar.vue';
+import Dashboard from './components/Dashboard.vue';
 </script>
 
 <template>
   <SideBar/>
   <MainBody>
     <Navbar/>
+    <Dashboard/>
   </MainBody>
 </template>
