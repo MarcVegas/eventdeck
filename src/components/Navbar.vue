fa2bcc3d12671e1370e133481d7510cab64dd61b
marcvegas <marcvegasprv@protonmail.com>
2022-07-25 08:41:42 +0800

    style: Removed new lines

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,9 +3,12 @@
 </script>
 
 <template>
-    <div class="h-20 border-gray-500 border-b-[1px] px-2 sm:px-4 py-4 overflow-hidden">
-        <div class="container flex flex-wrap justify-end items-center mx-auto">
-            <div class="flexitems-center md:order-2">
+    <div class="h-20 border-gray-500 border-b-[1px] px-4 lg:px-36 sm:px-4 py-4 overflow-hidden">
+        <div class="container flex flex-wrap justify-between items-center mx-auto">
+            <div>
+                <h1 class="text-white font-bold text-2xl">EVENT DECK</h1>
+            </div>
+            <div class="flex items-center md:order-2">
                 <button type="button"
                     class="flex mr-4 text-sm bg-gray-800 rounded-full  md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
                     id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown"
@@ -22,20 +25,6 @@
                             class="block text-sm font-medium text-gray-500 truncate dark:text-gray-400">name@flowbite.com</span>
                     </div>
                 </div>
-                <button data-collapse-toggle="mobile-menu-2" type="button"
-                    class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                    aria-controls="mobile-menu-2" aria-expanded="false">
-                    <span class="sr-only">Open main menu</span>
-                    <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
-                        xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd"
-                            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                            clip-rule="evenodd"></path>
-                    </svg>
-                </button>
-            </div>
-            <div class="hidden justify-between items-center w-full md:flex md:w-auto md:order-1" id="mobile-menu-2">
-                
             </div>
         </div>
     </div>
