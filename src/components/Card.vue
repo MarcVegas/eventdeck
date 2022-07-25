86775c89dd7ce946587fc5a2f254229635f1d48c
marcvegas <marcvegasprv@protonmail.com>
2022-07-25 21:13:52 +0800

    Added card and dashboard

---
+++ src/components/Card.vue
@@ -0,0 +1,12 @@
+<script setup>
+defineProps(['title'])
+
+</script>
+
+<template>
+<div class="p-6 basis-1/2 bg-[#343434] rounded-lg shadow-md">
+    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+        {{title}}</h5>
+    <slot></slot>
+</div>
+</template>
---
+++ src/components/Dashboard.vue
@@ -0,0 +1,32 @@
+<script setup>
+import Card from './Card.vue';
+
+const data = [
+  {name: 'Workout', data: {'2017-01-01 00:00:00 -0800': 3, '2017-01-02 00:00:00 -0800': 4}},
+  {name: 'Call parents', data: {'2017-01-01 00:00:00 -0800': 5, '2017-01-02 00:00:00 -0800': 3}}
+];
+</script>
+
+<template>
+<article class="lg:px-36 sm:px-4 py-4">
+<h1 class="font-bold text-2xl text-white mb-2">Dashboard</h1>
+<hr >
+<div class="mt-2">
+    <div class="flex flex-col gap-2 mt-4">
+        <div class="flex flex-row gap-4 mb-6">
+            <Card title="Line Chart" :class="max-w-lg">
+                <line-chart :data="{'2021-01-01': 11, '2021-01-02': 6}"></line-chart>
+            </Card>
+            <Card title="Bar Chart" :class="max-w-lg">
+                <bar-chart :data="[['Work', 32], ['Play', 1492]]"></bar-chart>
+            </Card>
+        </div>
+        <div class="flex ">
+            <Card title="Area Chart">
+                <area-chart :data="data"></area-chart>
+            </Card>
+        </div>
+    </div>
+</div>
+</article>
+</template>
