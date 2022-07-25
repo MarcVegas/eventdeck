b68c8a8a764e3202f7acd5b01f57bbfb1b5cc728
marcvegas <marcvegasprv@protonmail.com>
2022-07-25 21:15:50 +0800

    style: Change bg color on hover

--- src/components/SidebarButton.vue
+++ src/components/SidebarButton.vue
@@ -6,7 +6,7 @@
     <li class="w-full">
         <button
             class="flex items-center sm:justify-center h-16 no-underline transition-colors ease-in
-            focus:border-2 rounded-lg focus:bg-[#40B67E] focus:border-white">
+            focus:border-2 rounded-lg focus:bg-[#40B67E] focus:border-white hover:bg-[#343434]">
             <slot></slot>
         </button>
     </li>
