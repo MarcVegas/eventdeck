74c21162c0b9af40ae0b1ad02e5da481956ca9dd
marcvegas <marcvegasprv@protonmail.com>
2022-07-26 21:01:55 +0800

    feat: Created routes file to be used for vue router

---
+++ src/routes.js
@@ -0,0 +1,9 @@
+import Dashboard from './components/Dashboard.vue'
+import Events from './components/Events.vue'
+
+const routes = [
+    { path: '/', component: Dashboard},
+    { path: '/events', component: Events}
+]
+
+export default routes
