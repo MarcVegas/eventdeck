371bf736c7aea2cfc727bd846d83c72a7214e8e5
marcvegas <marcvegasprv@protonmail.com>
2022-07-26 21:50:28 +0800

    style: Replaced logo

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -5,24 +5,28 @@ import SidebarButton from './SidebarButton.vue';
 </script>
 
 <template>
-<nav class="fixed border-gray-500 border-t-[1px] bg-[#1C1C1C] lg:border-r-[1px] bottom-0 w-full h-20 
+    <nav class="fixed border-gray-500 border-t-[1px] bg-[#1C1C1C] lg:border-r-[1px] bottom-0 w-full h-20 
     lg:h-full lg:top-0 lg:w-20 overflow-auto">
-    <ul class="flex lg:flex-col flex-row gap-[1em] list-none p-2 m-0 items-center h-full">
-        <SidebarButton>
-            <ViewDashboard :class="['mx-[1.2rem]']" fillColor="#fff"/>
-        </SidebarButton>
-        <SidebarButton>
-            <Feather :class="['mx-[1.2rem]']" fillColor="#fff"/>
-        </SidebarButton>
-        <SidebarButton>
-            <Poll :class="['mx-[1.2rem]']" fillColor="#fff"/>
-        </SidebarButton>
-        <SidebarButton>
-            <AccountCog :class="['mx-[1.2rem]']" fillColor="#fff"/>
-        </SidebarButton>
-        <SidebarButton>
-            <TuneVariant :class="['mx-[1.2rem]']" fillColor="#fff"/>
-        </SidebarButton>
-    </ul>
-</nav>
+        <ul class="flex lg:flex-col flex-row gap-[1em] list-none p-2 m-0 items-center h-full">
+            <router-link to="/">
+                <SidebarButton>
+                    <ViewDashboard :class="['mx-[1.2rem]']" fillColor="#fff" />
+                </SidebarButton>
+            </router-link>
+            <router-link to="/events">
+                <SidebarButton>
+                    <Feather :class="['mx-[1.2rem]']" fillColor="#fff" />
+                </SidebarButton>
+            </router-link>
+            <SidebarButton>
+                <Poll :class="['mx-[1.2rem]']" fillColor="#fff" />
+            </SidebarButton>
+            <SidebarButton>
+                <AccountCog :class="['mx-[1.2rem]']" fillColor="#fff" />
+            </SidebarButton>
+            <SidebarButton>
+                <TuneVariant :class="['mx-[1.2rem]']" fillColor="#fff" />
+            </SidebarButton>
+        </ul>
+    </nav>
 </template>
